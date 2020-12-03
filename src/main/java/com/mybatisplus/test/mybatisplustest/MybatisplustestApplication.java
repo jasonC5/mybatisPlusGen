404521cd5d34534d31a4261dabf727e288ba6c25
JasonC5 <chenj@migrsoft.com>
2020-12-03 12:06:13 +0800

    mybatis-plus-generate 测试

---
+++ src/main/java/com/mybatisplus/test/mybatisplustest/MybatisplustestApplication.java
@@ -0,0 +1,13 @@
+package com.mybatisplus.test.mybatisplustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplustestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplustestApplication.class, args);
+    }
+
+}
