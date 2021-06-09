2dbfbd3d94210ca8b07a24c629e038e18b61dcf1
JasonC5 <chenj@migrsoft.com>
2021-06-09 09:58:51 +0800

    数据库路径修改

--- src/test/java/com/mybatisplus/test/mybatisplustest/TestAutoGenerate.java
+++ src/test/java/com/mybatisplus/test/mybatisplustest/TestAutoGenerate.java
@@ -23,11 +23,11 @@ public class TestAutoGenerate {
         // 拼接出代码最终输出的目录
         gc.setOutputDir(projectPath + "/src/main/java");
         // 配置开发者信息（可选）（需要修改）
-        gc.setAuthor("lyh");
+        gc.setAuthor("chenj");
         // 配置是否打开目录，false 为不打开（可选）
         gc.setOpen(false);
         // 实体属性 Swagger2 注解，添加 Swagger 依赖，开启 Swagger2 模式（可选）
-        //gc.setSwagger2(true);
+        gc.setSwagger2(true);
         // 重新生成文件时是否覆盖，false 表示不覆盖（可选）
         gc.setFileOverride(false);
         // 配置主键生成策略，此处为 ASSIGN_ID（可选）
@@ -41,7 +41,7 @@ public class TestAutoGenerate {
         // Step3：数据源配置（需要修改）
         DataSourceConfig dsc = new DataSourceConfig();
         // 配置数据库 url 地址
-        dsc.setUrl("jdbc:mysql://10.30.94.8:3306/dxh_mem?characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true");
+        dsc.setUrl("jdbc:mysql://10.174.89.47:3306/dxh_finance?characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true");
         // dsc.setSchemaName("testMyBatisPlus"); // 可以直接在 url 中指定数据库名
         // 配置数据库驱动
         dsc.setDriverName("com.mysql.jdbc.Driver");
@@ -56,7 +56,7 @@ public class TestAutoGenerate {
         // 配置父包名（需要修改）
         pc.setParent("com.dxh");
         // 配置模块名（需要修改）
-        pc.setModuleName("mem");
+        pc.setModuleName("finance");
         // 配置 entity 包名
         pc.setEntity("entity");
         // 配置 mapper 包名
@@ -70,20 +70,23 @@ public class TestAutoGenerate {
         // Step5：策略配置（数据库表配置）
         StrategyConfig strategy = new StrategyConfig();
         // 指定表名（可以同时操作多个表，使用 , 隔开）（需要修改）
-        strategy.setInclude("memPoint");
+//        strategy.setInclude("skuDistInfo","skuOtherInfo");
+        strategy.setInclude("trusteeshipSeparateSetting");
         // 配置数据表与实体类名之间映射的策略
-        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setNaming(NamingStrategy.no_change);//underline_to_camel   下划线转驼峰命名
         // 配置数据表的字段与实体类的属性名之间映射的策略
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.no_change);//underline_to_camel   下划线转驼峰命名
         // 配置 lombok 模式
         strategy.setEntityLombokModel(true);
         // 配置 rest 风格的控制器（@RestController）
         strategy.setRestControllerStyle(true);
         // 配置驼峰转连字符
-        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setControllerMappingHyphenStyle(false);
+        // 实体生成字段注解
+        strategy.setEntityTableFieldAnnotationEnable(true);
         // 配置表前缀，生成实体时去除表前缀
         // 此处的表名为 test_mybatis_plus_user，模块名为 test_mybatis_plus，去除前缀后剩下为 user。
-        strategy.setTablePrefix(pc.getModuleName() + "_");
+//        strategy.setTablePrefix(pc.getModuleName() + "_");
         mpg.setStrategy(strategy);
 
         // Step6：执行代码生成操作
